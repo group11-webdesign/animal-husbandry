e12f2567ad13f530c2e4514d389f1b9e0610d920
anonymouslove <118839427+anonymouslove@users.noreply.github.com>
2023-11-23 21:27:49 +0100

    hmm

---
+++ AgroAllies/sign/log.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: ../general/general.html");
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>login</title>
+  <link rel="stylesheet" href="../css/style_log.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <div class="container2">
+    <div class="image-container">
+      <img src="../assets/images/vector2.png" alt="Welcome Image">
+      <div class="text-overlay">Providing Animal Health Solution</div>
+    </div>
+  </div>
+  <div class="container">
+  <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+        <form action="g.php" method="post">
+    <div class="title">Create Account</div>
+    
+      
+      <div class="user-details2">
+        <div class="input-box">
+          <span class="details">Email</span>
+          <input type="email" placeholder="Email" name="email">
+         
+        </div>
+
+     
+        <div class="input-box">
+          <span class="details">Passsword</span>
+          <input type="password" placeholder="Password" name="password" >
+          
+        </div>
+      </div>
+
+      <div class="button">
+        <input type="submit" value="Login"  name="login" >
+        
+      
+      </div>
+      <div class="register-link">
+      <p>Don't have an account?<a href="../sign/sign.php">SignUp</a></p>
+      </div>
+
+     
+    </form>
+  </div>
+</body>
+
+</html>
